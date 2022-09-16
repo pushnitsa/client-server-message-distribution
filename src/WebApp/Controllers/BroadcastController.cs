410717933375447eab9d82639ce93ccc12505e99
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-09-16 13:50:32 +0200

    Add controller with broadcast method

---
+++ src/WebApp/Controllers/BroadcastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Route("api/broadcast")]
+[ApiController]
+public class BroadcastController : ControllerBase
+{
+    public BroadcastController()
+    {
+
+    }
+
+    [HttpGet("send")]
+    public IActionResult SendMessageToClients()
+    {
+
+        return Ok();
+    }
+}
+
