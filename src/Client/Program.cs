7200aab7b11fded4647e9b0a87e128c9a53a490e
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-09-19 12:05:06 +0200

    Add configuration dependency

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,7 +1,17 @@
 ﻿using Client.Chat;
+using Microsoft.Extensions.Configuration;
 
-var chatClient = new ChatClient();
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+int.TryParse(configuration["Port"], out var port);
+var host = configuration["Host"];
+
+var chatClient = new ChatClient(host, port);
 
 chatClient.Start();
 
 Console.ReadLine();
+
+
